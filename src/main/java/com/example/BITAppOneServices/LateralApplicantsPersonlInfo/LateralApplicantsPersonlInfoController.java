ce108653eb4365e78a77c54560c45d79a4ed1394
bitmis <pmg@ucsc.cmb.ac.lk>
2022-08-16 15:45:30 +0530

    application status II

--- src/main/java/com/example/BITAppOneServices/LateralApplicantsPersonlInfo/LateralApplicantsPersonlInfoController.java
+++ src/main/java/com/example/BITAppOneServices/LateralApplicantsPersonlInfo/LateralApplicantsPersonlInfoController.java
@@ -35,8 +35,6 @@ public class LateralApplicantsPersonlInfoController {
     }
 
 
-
-
     // Read operation
     @GetMapping("/get_lateral_applicant_info/{application_no}")
     public LateralApplicantsPersonlInfo fetchLateralApplicantsPersonalInfo( @PathVariable("application_no") String application_no )
--- src/main/java/com/example/BITAppOneServices/MasterALSubjects/MasterALSubjectsController.java
+++ src/main/java/com/example/BITAppOneServices/MasterALSubjects/MasterALSubjectsController.java
@@ -1,11 +1,15 @@
 package com.example.BITAppOneServices.MasterALSubjects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
+@RequestMapping(value = "/api")
 @RestController
 public class MasterALSubjectsController {
 
