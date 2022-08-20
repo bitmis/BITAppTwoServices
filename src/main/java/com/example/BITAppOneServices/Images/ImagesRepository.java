5f429d90329b684bd3a200c38e93170797b621b8
bitmis <pmg@ucsc.cmb.ac.lk>
2022-08-20 21:37:01 +0530

    image save I

--- src/main/java/com/example/BITAppOneServices/Images/ImagesRepository.java
+++ src/main/java/com/example/BITAppOneServices/Images/ImagesRepository.java
@@ -1,4 +1,11 @@
 package com.example.BITAppOneServices.Images;
 
-public interface ImagesRepository {
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+   public interface ImagesRepository extends JpaRepository<ImageModel, Long> {
+        Optional<ImageModel> findByName(String name);
+    }
+
