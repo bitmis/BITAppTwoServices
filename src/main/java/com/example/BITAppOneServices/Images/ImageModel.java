b03238517f4f3fe601d734969ce5351ae146713b
bitmis <pmg@ucsc.cmb.ac.lk>
2022-09-08 16:41:20 +0530

    application download I

--- src/main/java/com/example/BITAppOneServices/Images/ImageModel.java
+++ src/main/java/com/example/BITAppOneServices/Images/ImageModel.java
@@ -20,6 +20,15 @@ public class ImageModel {
         this.type = type;
         this.picByte = picByte;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/example/BITAppOneServices/LateralApplicantsPersonlInfo/ApplicationDownload/ApplicationDownloadController.java
@@ -0,0 +1,10 @@
+package com.example.BITAppOneServices.LateralApplicantsPersonlInfo.ApplicationDownload;
+
+public class ApplicationDownloadController {
+
+   public void myhtml(){
+
+
+       String mystring = "<table style=\"border:0;background-color:#424242;width:100%;margin-bottom:3%\"><tr style=\"background-color:#fff\"><td><span>1. Name in Full:</span><br><span style=\"padding-left:10%\">aaaaaaaaeeeeeeeeeeeeeeeeeee</span><br><span>2. Name with Initials:</span><br><span style=\"padding-left:10%\">aaaaaaaaeeeeeeeeeeeeeeeeeee</span></td></table><table style=\"border:0;background-color:#424242;width:100%;margin-bottom:3%\"><tr style=\"background-color:#fff;border:0 #fff\"><td style=\"border:0\"><span style=\"padding-top:5%\">3. Title:</span><br><span style=\"padding-left:10%;color:red\">Female</span></td><td style=\"border:0\"><span style=\"padding-top:5%\">4. Gender:</span><br><span style=\"padding-left:10%;color:red\">Ms.</span></td><td style=\"border:0\"><span style=\"padding-top:5%\">5. Citizenship:</span><br><span style=\"padding-left:10%;color:red\">Sri Lankan</span></td><td style=\"border:0\"><span style=\"padding-top:5%\">6. Nationality:</span><br><span style=\"padding-left:10%;color:red\">Sri Lankan</span></td></tr></table><table style=\"border:0;background-color:#424242;width:100%;margin-bottom:3%\"><tr style=\"background-color:#fff\"><td><span style=\"padding-top:5%\">7. National ID No:</span><br><span style=\"padding-left:10%;color:red\">kkkkkkkkk</span><br><span>8. Date of Birth:</span><br><span style=\"padding-left:10%\">aaaaaaaaeeeeeeeeeeeeeeeeeee</span></td></tr></table><table style=\"border:0;background-color:#424242;width:100%;margin-bottom:3%\"><tr style=\"background-color:#fff\"><td><span>10. Contact Details:</span><br><span style=\"padding-left:10%\">(a)Permanant Address</span><br><span style=\"padding-left:10%\">(b)Country</span><br><span style=\"padding-left:10%\">(c)Mobile</span></td></tr></table>";
+   }
+}
